5a527006f60edf9ee7caeefc44f6fb0a39c0ae4b
Marko <neonmarko2@gmail.com>
2025-11-08 13:33:31 +0100

    Create udimen
    I need a better way to manage positioning and sizing of ui elements. At
    some point I'd like to be able to not just position the objects based on
    pixel units, but also position them relative to their parent. The same
    way roblox has udims with scale and offset values

---
+++ editor_utils/gui/udimen.lua
@@ -0,0 +1,23 @@
+local udimen = {}
+
+local udimen_meta = {}
+udimen_meta.__index = udimen_meta
+
+---@class Udimen Stands for User Interface Dimension
+---@field x_relative number
+---@field y_relative number
+---@field x_absolute number
+---@field y_absolute number
+
+---@return Udimen
+function udimen.new(x_relative, y_relative, x_absolute, y_absolute)
+	x_relative = x_relative or 0
+	y_relative = y_relative or 0
+	x_absolute = x_absolute or 0
+	y_absolute = y_absolute or 0
+	local udimen =
+		{ x_relative = x_relative, y_relative = y_relative, x_absolute = x_absolute, y_absolute = y_absolute }
+	return setmetatable(udimen, udimen_meta)
+end
+
+return udimen
